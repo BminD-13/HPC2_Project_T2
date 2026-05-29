ff7b079c0c99b66dea322a17b84e7251497cbf80
Benjamin_Daiber <benjamin.daiber@uni-ulm.de>
2026-05-29 11:37:04 +0200

    add: circle integration

--- main.m
+++ main.m
@@ -8,8 +8,11 @@ close all
 mesh
 
 % Kreis
-theta = linspace(0,2*pi,200);
-circle = [3 + 2*cos(theta); 3 + 2*sin(theta)];
+radius = 2;
+cx = 2.5; cy = -1.5;
+N = 2000;
+theta = linspace(0,2*pi,N);
+circle = [cx + radius*cos(theta); cy + radius*sin(theta)]';
 
 %% Randbedingungen
 
@@ -20,13 +23,11 @@ g  = @(x) zeros(size(x,1),1);
 nodes5 = unique(boundary{6}(:));
 nodes3 = unique(boundary{13}(:));
 nodes7 = unique([boundary{14}(:);boundary{15}(:)]);
-
-% Zugehörige Koordinaten
 coords5 = coordinates(nodes5,:);
 coords3 = coordinates(nodes3,:);
 coords7 = coordinates(nodes7,:);
 
-uD = @(x) uDiriclet(x, coords5, coords3, coords7); % am Ende des Skripts
+uD = @(x) uDiriclet(x, coords5, coords3, coords7);
 
 %% PDE Solver
 
@@ -34,18 +35,24 @@ uD = @(x) uDiriclet(x, coords5, coords3, coords7); % am Ende des Skripts
 
 %% Integral über den Kreis
 
+ds = 2*pi*radius / (N-1);
 
+summe = 0;
 
-% Chat GPT Anwort:
+for i = 1:N-1
+    p = circle(i,:);
+    [P,U] = findTriangle(p, coordinates, elements, x);
+    update = evalP1(p(1),p(2),P,U);
+    summe = summe + update;
+end
 
-% Bei P1-FEM (lineare Dreieckselemente) gilt auf jedem Dreieck:
-% lineare Basisfunktionen (hat functions, baryzentrische Koordinaten)
+integral = summe * ds; % 54-81
 
-% Das nennt man:
-% Finite-Element-Rekonstruktion
-% Finite-Element-Interpolation
-% P1-Interpolant
-% Piecewise linear interpolation (stückweise lineare Interpolation)
+% Analytische Abschätzung:
+% MinMax Werte laut Grafik sind ca. 4.3 und 6.5 und Radius = 2
+% Imin = 2 * r * pi * min = 2 * 2 * pi * 4.3 = 54
+% Imax = 2 * r * pi * max = 2 * 2 * pi * 6.5 = 81
+
 
 %% Plot
 
@@ -62,33 +69,89 @@ xlabel('x');
 ylabel('y');
 zlabel('u_h');
 
+% Kreis
+hold on
+plot(circle(:,1), circle(:,2), 'r', 'LineWidth', 2)
+hold off
+
+
 disp(['Energie: ', num2str(Energie)]);
 
 %% Funktionen
 
+% Randbedingung
 function W = uDiriclet(x,coords5,coords3,coords7)
 
 tol = 1e-10;
-
 n = size(x,1);
-
 W = zeros(n,1);
 
 for i = 1:n
 
     if any(vecnorm(coords5 - x(i,:),2,2) < tol)
-
         W(i) = 5;
 
     elseif any(vecnorm(coords3 - x(i,:),2,2) < tol)
-
         W(i) = 3;
 
     elseif any(vecnorm(coords7 - x(i,:),2,2) < tol)
-
         W(i) = 7;
 
     end
 end
 
+end
+
+% P1 Interpolation
+
+function inside = pointInTriangle(p, P)
+
+A = [
+    1      1      1;
+    P(1,1) P(2,1) P(3,1);
+    P(1,2) P(2,2) P(3,2)
+];
+
+rhs = [1; p(1); p(2)];
+
+lambda = A \ rhs;
+
+inside = all(lambda >= -1e-12) && all(lambda <= 1+1e-12);
+
+end
+
+
+function val = evalP1(x,y,P,U)
+
+A = [
+    1 1 1;
+    P(:,1)';
+    P(:,2)'
+];
+
+rhs = [1; x; y];
+
+lambda = A \ rhs;
+
+val = lambda' * U;
+
+end
+
+function [P,U] = findTriangle(p, coordinates, elements, u)
+
+for k = 1:size(elements,1)
+
+    nodes = elements(k,:);
+    Pk = coordinates(nodes,:);
+    
+    if pointInTriangle(p, Pk)
+
+        P = Pk;
+        U = u(nodes);
+        return;
+
+    end
+end
+
+error('Point not in mesh domain');
 end
